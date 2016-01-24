bccbfe31f3a3b3cf07924e6902721c6c09e2fcee
AlexTs2002 <ostrietoxxx@gmail.com>
2016-01-24 13:56:48 +0200

    first func def

--- C and C++/tim_sort.hpp
+++ C and C++/tim_sort.hpp
@@ -1,7 +1,7 @@
 #ifndef tim_sort
 #define tim_sort
 
-void tim_sort ()
+template <typename RandomeAccessIterator, typename funcType> void tim_sort (RandomeAceessIterator begin, RandomeAccessIterator end, funcType comp)
 {
 
 }
