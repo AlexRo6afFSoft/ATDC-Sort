dd279c037bbc3f311611775a2baa0c8c78e1a75d
Ro6afF <ro6aff@drugel.motikahost>
2016-01-24 16:30:51 +0200

    Added abstract insetrion sort.

---
+++ C and C++/Insertion_Sort/insertion_sort.hpp
@@ -0,0 +1,20 @@
+#ifndef insertion_sort
+#define insetrion_sort
+
+	template<typename T>
+void insertion_sort(T begin, T end)
+{
+	for(int i=1; i<5; i++)
+	{
+		int index = array[i];
+		int dec = i;
+		while(dec>0 && array[dec-1]>=index)
+		{
+			array[dec]=array[dec-1];
+			--dec;
+		}
+		array[dec]=index;
+	}
+}
+
+#endif
