50792308b4a3519657395a55cedc0b8cc31c975d
Alex Tsvetanov <ostrietoxxx@gmail.com>
2016-04-07 14:30:40 +0300

    Add Compare function

--- C and C++/README.md
+++ C and C++/README.md
@@ -1,4 +1,6 @@
 #ATDC Sorting Algorithm
 ##Usage 
 1. Add **_#include "atdc_sort.cpp"_**
-2. To sort add **atdc_sort(_Itterator to the begining sorting sequence_, _Itterator to ending of sorting sequense_)**
+2. To sort add 
+   1. **atdc_sort(_Iterator to the begining sorting sequence_, _Iterator to ending of sorting sequense_)** or
+   2. **atdc_sort(_Iterator to the begining sorting sequence_, _Iterator to ending of sorting sequense_, _Compare function_)**
--- C and C++/atdc_sort.cpp
+++ C and C++/atdc_sort.cpp
@@ -21,15 +21,59 @@ void __merge (std::iterator_traits <T>, std::iterator_traits <T>, std::iterator_
 #endif
 
 
-template <typename T>
-void __merge (std::iterator_traits <T> b1, std::iterator_traits <T> e1, std::iterator_traits <T> b2, std::iterator_traits <T> e2)
+template <typename T, typename X>
+void __merge (std::iterator_traits <T> b1, std::iterator_traits <T> e1, std::iterator_traits <T> b2, std::iterator_traits <T> e2, X comp)
 {
-	std::vector <int> arr;
-
+	std::vector <T> arr;
+	std::iterator_traits <T> it1 = b1, it2 = b2;
+	while (it1 != e1 and it2 != e2)
+	{
+		if (it1 == e1)
+		{
+			arr.push_back (*it2);
+			it2 ++;
+		}
+		else if (it2 == e2)
+		{
+			arr.push_back (*it1);
+			it1 ++;
+		}
+		else
+		{
+			if (comp (*it1, *it2))
+			{
+				arr.push_back (*it1);
+				it1 ++;
+			}
+			else
+			{
+				arr.push_back (*it2);
+				it2 ++;
+			}
+		}
+	}
+	it1 = b1, it2 = b2;
+	for (size_t i = 0 ; i < arr.size () ; i ++)
+	{
+		if (it1 == e1)
+		{
+			*it2 = arr [i];
+			it2 ++;
+		}
+		else if (it2 == e2)
+		{
+			return ;
+		}
+		else
+		{
+			*it1 = arr [i];
+			it1 ++;
+		}
+	}
 }
 
-template <typename T>
-void __atdc_sort (std::iterator_traits <T> Begin, std::iterator_traits <T> End)
+template <typename T, typename X = less<T> >
+void __atdc_sort (std::iterator_traits <T> Begin, std::iterator_traits <T> End, X comp)
 {
 	int dist = std::distance (Begin, End);
 	if (dist == 0)
@@ -38,7 +82,7 @@ void __atdc_sort (std::iterator_traits <T> Begin, std::iterator_traits <T> End)
 		return ;
 	if (dist == 2)
 	{
-		if (*Begin > *prev (End))
+		if (!comp (*Begin, *prev (End)))
 			swap (*Begin, *End);
 		return ;
 	}
@@ -46,14 +90,14 @@ void __atdc_sort (std::iterator_traits <T> Begin, std::iterator_traits <T> End)
 	std::iterator_traits <T> mid = Begin + dist;
 	__atdc_sort (Begin, mid);
 	__atdc_sort (mid, End);
-	__merge (Begin, mid, std::next (mid), End);
+	__merge (Begin, mid, std::next (mid), End, comp);
 }
 
-template <typename T>
-void atdc_sort (std::iterator_traits <T> Begin, std::iterator_traits <T> End)
+template <typename T, typename X>
+void atdc_sort (std::iterator_traits <T> Begin, std::iterator_traits <T> End, X comp)
 {
 	if (std::iterator_traits <T>::iterator_category != std::random_access_iterator_tag())
 		throw std::invalid_argument ("Given iterator is not random access iterator");
 
-	__atdc_sort (Begin, End);
+	__atdc_sort (Begin, End, comp);
 }
