c9b8904da3cfe7307953b252c15ae6093530eaf3
Dimo Dimov Chanev <dimo_chanev@windowslive.com>
2016-04-16 11:25:44 +0300

    Update main.cs

--- C#/main.cs
+++ C#/main.cs
@@ -1,12 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using AlexRo6afF;
 
 static class ATDC_Sort
 {
     static Random rnd = new Random();
-    
     static void Main()
     {
         List<int> L = new List<int>();
@@ -16,12 +14,14 @@ static class ATDC_Sort
             Console.Write(L[i].ToString() + " ");
         }
         Console.WriteLine();
+
         ATDC.Sort((dynamic)L, 0, L.Count);
         for (int i = 0; i < 25; i++)
         {
             Console.Write(L[i].ToString() + " ");
         }
         Console.WriteLine();
+
         Comp<dynamic> c = (a, b) => { return b.CompareTo(a); };
         ATDC.Sort((dynamic)L, 0, L.Count, c);
         for (int i = 0; i < 25; i++)
