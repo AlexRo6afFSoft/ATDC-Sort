7a94e78d1466dae11ab35b73767efdce95d123b8
Dimo Dimov Chanev <dimo_chanev@windowslive.com>
2016-04-16 11:10:18 +0300

    Added example

--- C#/main.cs
+++ C#/main.cs
@@ -1,17 +1,31 @@
 using System;
 using System.Collections.Generic;
+using AlexRo6afF;
 
 static class ATDC_Sort
 {
-	static Random rnd = new Random();
-	static void Main()
-	{
-		List<int> L = new List<int> ();
-		for (int i = 0; i < 10; i++)
-		{
-			L.Add (rnd.Next (-100, 100));
-			Console.Write (L [i].ToString () + " ");
-		}
-		Console.WriteLine ();
-	}
+    static Random rnd = new Random();
+    static void Main()
+    {
+        List<int> L = new List<int>();
+        for (int i = 0; i < 10; i++)
+        {
+            L.Add(rnd.Next(-100, 100));
+            Console.Write(L[i].ToString() + " ");
+        }
+        Console.WriteLine();
+        ATDC.Sort((dynamic)L, 0, L.Count);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(L[i].ToString() + " ");
+        }
+        Console.WriteLine();
+        Comp<dynamic> c = (a, b) => { return b.CompareTo(a); };
+        ATDC.Sort((dynamic)L, 0, L.Count, c);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(L[i].ToString() + " ");
+        }
+        Console.WriteLine();
+    }
 }
