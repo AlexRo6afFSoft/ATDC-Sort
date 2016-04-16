2ddca436b6fd771edc9947f4845285caa922c682
Dimo Dimov Chanev <dimo_chanev@windowslive.com>
2016-04-16 10:33:04 +0300

    Clean up

--- C++/atdc_sort.cpp
+++ C++/atdc_sort.cpp
@@ -15,15 +15,10 @@
 template <typename RAI, typename X>
 void __merge (RAI b1, RAI e1, RAI b2, RAI e2, X comp)
 {
-//	if (RAI::iterator_category != std::random_access_iterator_tag())
-//		throw std::invalid_argument ("Given iterator is not random access iterator");
-
-	//std::cout << "ALL1 is OK " << distance (b1, e1) << " & " << distance (b2, e2) << std::endl;
 	std::vector <typename RAI::value_type> __arr;
 	RAI it1 = b1, it2 = b2;
 	while (it1 != e1 && it2 != e2)
 	{
-		//std::cout << "Comp: " << comp (*it1, *it2) << std::endl;
 		if (comp (*it1, *it2) == 1)
 		{
 			__arr.push_back (*it1);
@@ -35,7 +30,7 @@ void __merge (RAI b1, RAI e1, RAI b2, RAI e2, X comp)
 			it2 ++;
 		}
 	}
-	//std::cout << "\tALL1 is OK " << distance (b1, e1) << " & " << distance (b2, e2) << std::endl;
+	
 	if (it1 == e1)
 	{
 		while (it2 != e2)
@@ -52,14 +47,14 @@ void __merge (RAI b1, RAI e1, RAI b2, RAI e2, X comp)
 			it1 ++;
 		}
 	}
-	//std::cout << "\t\tALL1 is OK " << distance (b1, e1) << " & " << distance (b2, e2) << std::endl;
+	
 	it1 = b1;
 	for (size_t i = 0 ; i < (size_t)distance (b1, e1) ; i ++)
 	{
 		*it1 = __arr [i];
 		it1 ++;
 	}
-	//std::cout << "\t\t\tALL1 is OK " << distance (b1, e1) << " & " << distance (b2, e2) << std::endl;
+	
 	it2 = b2;
 	for (size_t i = (size_t)distance (b1, e1) ; i < __arr.size () ; i ++)
 	{
@@ -71,10 +66,6 @@ void __merge (RAI b1, RAI e1, RAI b2, RAI e2, X comp)
 template <typename RAI, typename X = std::less<typename RAI::value_type> >
 void __atdc_sort (RAI Begin, RAI End, X comp)
 {
-////if (RAI::iterator_category != std::random_access_iterator_tag())
-////	throw std::invalid_argument ("Given iterator is not random access iterator");
-	//std::cout << "ALL2 is OK " << distance (Begin, End) << std::endl;
-
 	int dist = std::distance (Begin, End);
 	if (dist == 0)
 		return ;
@@ -91,6 +82,7 @@ void __atdc_sort (RAI Begin, RAI End, X comp)
 		}
 		return ;
 	}
+	
 	dist /= 2;
 	RAI mid = Begin + dist;
 	__atdc_sort (Begin, mid, comp);
@@ -101,10 +93,6 @@ void __atdc_sort (RAI Begin, RAI End, X comp)
 template <typename RAI, typename X = std::less<typename RAI::value_type> >
 void atdc_sort (RAI Begin, RAI End, X comp = X())
 {
-////if (RAI::iterator_category != std::random_access_iterator_tag())
-////	throw std::invalid_argument ("Given iterator is not random access iterator");
-	//std::cout << "ALL3 is OK " << distance (Begin, End) << std::endl;
-
 	__atdc_sort (Begin, End, comp);
 }
 #endif
