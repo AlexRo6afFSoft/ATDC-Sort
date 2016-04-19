3e9851a635530e0f34a3d65f0843be9a29bd8f4a
Alex Tsvetanov <ostrietoxxx@gmail.com>
2016-04-19 23:07:26 +0300

    MONO_version & Microsoft_version

---
+++ C#/MONO_version/atdc_sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.CSharp;
+using System.Collections.Generic;
+using AlexRo6afF;
+
+static class ATDC_Sort
+{
+	static Random rnd = new Random();
+	static void Main()
+	{
+		List<int> L = new List<int>();
+		for (int i = 0; i < 25; i++)
+		{
+			L.Add(rnd.Next(-100, 100));
+			Console.Write(L[i].ToString() + " ");
+		}
+		Console.WriteLine();
+
+		ATDC.Sort((dynamic)L, 0, L.Count);
+		for (int i = 0; i < 25; i++)
+		{
+			Console.Write(L[i].ToString() + " ");
+		}
+		Console.WriteLine();
+
+		Comp<dynamic> c = (a, b) => { return b.CompareTo(a); };
+		ATDC.Sort((dynamic)L, 0, L.Count, c);
+		for (int i = 0; i < 25; i++)
+		{
+			Console.Write(L[i].ToString() + " ");
+		}
+		Console.WriteLine();
+	}
+}
